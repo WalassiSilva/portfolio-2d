d2f4a5241301208486bfa9e9bd42072d3e07dfeb
Walassi Silva <walassi.b@gmail.com>
2024-04-17 00:27:37 -0300

    first commit

---
+++ src/constants.js
@@ -0,0 +1,42 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC, after work, is here that I spend most of my time studying and doing some projects.`,
+
+  tv: `Here on my TV, I watch YouTube content from 
+  DevQuest, 
+  RocketSeat, 
+  Sujeito Programador, 
+  FreeCodeCamp, 
+  ByteGrad, 
+  Matheus Battisti and
+  Crunchyroll for entertainment.`,
+  
+  bed: `This is where I sleep. Rest is really important part of learning. But I often dream with code.`,
+  
+  courses: `Here you can see the technologies that I have learned:
+  React, TypeScript, JavaScript, Tailwind, SASS, Styled Components, NodeJS, Express, PostgreSQL, HTML, CSS, Java and C#`,
+  
+  "sofa-table":"That is my sofa. I like to play some cards, solve Rubik's Cube, talk with my friends. Here is where I relax.",
+  
+  resume: `At my desk, I have a resume. You can see it here: <br>
+  <a href="https://github.com/WalassiSilva/Resume/blob/main/CV_PT-BR.pdf" target="_blank" rel="noopener noreferrer">CV_PT-BR</a> <br>
+  <a href="https://github.com/WalassiSilva/Resume/blob/main/CV_EN.pdf" target="_blank" rel="noopener noreferrer">CV_EN</a>
+  `,
+
+  
+  library: `Here you can see a lot of fantasy books, but I prefer to watch something on my TV.`,
+  
+  projects: `You can check my projects like:
+   here on my 
+   <a href="https://github.com/WalassiSilva/money-manager" target="_blank" rel="noopener noreferrer">Money Manager, </a>
+   <a href="https://github.com/WalassiSilva/node-api-NLW" target="_blank" rel="noopener noreferrer">Node API, </a>
+   <a href="https://github.com/WalassiSilva/node-api-NLW" target="_blank" rel="noopener noreferrer">PokeApi. </a>
+   Take a look at my 
+   <a href="https://github.com/WalassiSilva/" target="_blank" rel="noopener noreferrer">Github.</a>
+   `,
+  
+  exit: `Just close the tab to Exit`,
+
+
+}
